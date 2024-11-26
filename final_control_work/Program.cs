72ddc697e780087f628a0b286f8fe1ce388d7ba7
Alina Duhu <metalyugina12@icloud.com>
2024-11-26 10:38:23 +0300

    Add decision

--- final_control_work/Program.cs
+++ final_control_work/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] input = { "1234", "1567", "-2", "computer science" };
+int max = 3;
+ 
+string[] result = new string[input.Length];
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i].Length <= max)
+
+    {
+        result[i] = input[i];
+    }
+
+}
+foreach (string resultItem in result)
+{
+
+    Console.WriteLine(resultItem);
+}
+
